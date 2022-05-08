cacb678a1cb338e91110724080b28d7758ff9ef1
NAZADOTH <108572837+NAZADOTH@users.noreply.github.com>
2022-05-08 22:50:20 +0200

    More stuff

---
+++ api.py
@@ -0,0 +1,61 @@
+import datetime
+import os
+import hashlib
+import time
+
+import requests
+
+
+class Api:
+    base_url = "https://api.smitegame.com/smiteapi.svc"
+
+    def __init__(
+        self,
+        dev_id: str = os.getenv("SMITE_DEV_ID"),
+        auth_key: str = os.getenv("SMITE_AUTH_KEY"),
+        delay: datetime.timedelta | None = datetime.timedelta(milliseconds=100),
+    ):
+        self.dev_id = dev_id
+        self.auth_key = auth_key
+        self.delay = delay
+        self.last: datetime.datetime | None = None
+        self.session: str | None = None
+        assert self.dev_id and self.auth_key
+
+    @staticmethod
+    def ping():
+        return requests.get(f"{Api.base_url}/pingjson")
+
+    def create_signature(self, method_name: str, timestamp: str):
+        return hashlib.md5(
+            f"{self.dev_id}{method_name}{self.auth_key}{timestamp}".encode("utf8")
+        ).hexdigest()
+
+    def _call_method(self, method_name: str, *args):
+        now = datetime.datetime.now(datetime.timezone.utc)
+        if self.delay is not None:
+            if self.last is None or self.last + self.delay <= now:
+                self.last = now
+            else:
+                self.last += self.delay
+                to_sleep = self.last + self.delay - now
+                time.sleep(to_sleep.total_seconds())
+
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        signature = self.create_signature(method_name, timestamp)
+        url = (
+            f"{self.base_url}/{method_name}json/{self.dev_id}/{signature}/"
+            + (f"{self.session}/" if self.session else "")
+            + timestamp
+        )
+        for arg in args:
+            url += f"/{arg}"
+        return requests.get(url)
+
+    def create_session(self):
+        self.session = self._call_method("createsession").json()["session_id"]
+
+    def call_method(self, *args):
+        if self.session is None:
+            self.create_session()
+        return self._call_method(*args)
--- hog.py
+++ hog.py
@@ -1,16 +1,11 @@
 import curses
 import curses.ascii
 from typing import *
-import time
 
+from api import Api
 
-class PlayerInfo(TypedDict):
-    mmr: str
-    hours: str
-    created: str
-    status: str
-    gods: List["GodInfo"]
-    matches: List["MatchInfo"]
+skipped_names = ["Siemka4", "kapitán"]
+api = Api()
 
 
 class GodInfo(TypedDict):
@@ -28,93 +23,172 @@ class MatchInfo(TypedDict):
     kda: str
 
 
-def call_hirez_api(name: str) -> PlayerInfo:
-    # time.sleep(1)
-    return {
-        "mmr": "1500",
-        "hours": "30",
-        "created": "yesterday",
-        "status": "",
-        "gods": [
-            {
-                "name": "He Bo",
-                "matches": "10",
-                "wr": "60%",
-                "last": "today",
-            },
-        ],
-        "matches": [
-            {
-                "outcome": "win",
-                "length": "30m",
-                "role": "mid",
-                "god": "He Bo",
-                "kda": "3/1/3",
-            },
-        ],
-    }
+class PlayerInfo(TypedDict):
+    mmr: str
+    hours: str
+    created: str
+    status: str
+    gods: List[GodInfo]
+    matches: List[MatchInfo]
 
 
-def redraw_panel(spaces: int, name: str, player: PlayerInfo, panel=curses.initscr()):
+class Player(TypedDict):
+    name: str
+    info: Optional[PlayerInfo]
+
+
+def call_hirez_api(player: str) -> PlayerInfo | None:
+    getplayer_resp = api.call_method("getplayer", player)
+    getplayer_resp.raise_for_status()
+
+    getplayer_json = getplayer_resp.json()
+    if not getplayer_json:
+        return None
+    x = getplayer_json[0]
+
+    res = {}
+    res["name"] = player
+    res["mmr"] = f"{x['Rank_Stat_Conquest']:.0f}"
+    res["hours"] = str(x["HoursPlayed"])
+    res["created"] = str(x["Created_Datetime"])
+    res["status"] = str(x["Personal_Status_Message"])
+
+    getqueuestats_resp = api.call_method("getqueuestats", player, "451")
+    getqueuestats_resp.raise_for_status()
+
+    getqueuestats_json = getqueuestats_resp.json()
+    res["gods"] = [
+        {
+            "name": x["God"],
+            "matches": str(x["Matches"]),
+            "wr": f"{x['Wins'] / x['Matches']:.0%}",
+            "last": x["LastPlayed"],
+        }
+        for x in getqueuestats_json[:3]
+    ]
+
+    getmatchhistory_resp = api.call_method("getmatchhistory", player)
+    getmatchhistory_resp.raise_for_status()
+
+    xx = getmatchhistory_resp.json()
+    xx = [x for x in xx if x["Match_Queue_Id"] == 451]
+    res["matches"] = [
+        {
+            "outcome": x["Win_Status"],
+            "length": f"{x['Minutes']}m",
+            "role": x["Role"],
+            "god": x["God"],
+            "kda": f"{x['Kills']}/{x['Deaths']}/{x['Assists']}",
+        }
+        for x in xx[:3]
+    ]
+
+    return res
+    # return {
+    #     "mmr": "1500",
+    #     "hours": "30",
+    #     "created": "yesterday",
+    #     "status": "x" * 120,
+    #     "gods": [
+    #         {
+    #             "name": "He Bo",
+    #             "matches": "10",
+    #             "wr": "60%",
+    #             "last": "today",
+    #         },
+    #     ],
+    #     "matches": [
+    #         {
+    #             "outcome": "win",
+    #             "length": "30m",
+    #             "role": "mid",
+    #             "god": "He Bo",
+    #             "kda": "3/1/3",
+    #         },
+    #     ],
+    # }
+
+
+def wrap_str(y: int, x: int, s: str, spaces: int, panel=curses.initscr()):
+    _, max_x = panel.getmaxyx()
+    curr_s = s
+    y_inc = 0
+    while True:
+        if x + len(curr_s) < max_x:
+            panel.addstr(y + y_inc, x, curr_s)
+            return y_inc + 1
+        offset = max_x - x - 1 - 1
+        panel.addstr(y + y_inc, x, curr_s[:offset] + "-")
+        curr_s = curr_s[offset:]
+        if y_inc == 0:
+            x += spaces
+        y_inc += 1
+
+
+def redraw_panel(spaces: int, player: Player, panel=curses.initscr()):
     panel.clear()
-    panel.addstr(name)
+    panel.addstr(player["name"])
+    if player["info"] is None:
+        panel.addstr(1, spaces, "not found")
+        panel.refresh()
+        return
     row = 1
-    panel.addstr(row, spaces, "MMR: " + player['mmr'])
+    panel.addstr(row, spaces, "MMR: " + player["info"]["mmr"])
     row += 1
-    panel.addstr(row, spaces, "Hours played: " + player["hours"])
+    panel.addstr(row, spaces, "Hours played: " + player["info"]["hours"])
     row += 1
-    panel.addstr(row, spaces, "Account creation date: " + player["created"])
+    panel.addstr(row, spaces, "Created: " + player["info"]["created"])
     row += 1
-    panel.addstr(row, spaces, "Status message: " + player["status"])
-    row += 1
+    row += wrap_str(row, spaces, "Status: " + player["info"]["status"], spaces, panel)
     panel.addstr(row, spaces, "Most played gods (in ranked conquest)")
     row += 1
-    for god in player['gods']:
-        panel.addstr(row, 2 * spaces, god['name'])
+    for god in player["info"]["gods"]:
+        panel.addstr(row, 2 * spaces, god["name"])
         row += 1
-        panel.addstr(row, 3 * spaces, "Matches played: " + god['matches'])
+        panel.addstr(row, 3 * spaces, "Matches played: " + god["matches"])
         row += 1
-        panel.addstr(row, 3 * spaces, "WR: " + god['wr'])
+        panel.addstr(row, 3 * spaces, "WR: " + god["wr"])
         row += 1
-        panel.addstr(row, 3 * spaces, "Last played date: " + god['last'])
+        panel.addstr(row, 3 * spaces, "Last: " + god["last"])
         row += 1
     panel.addstr(row, spaces, "Recent matches (in ranked conquest)")
     row += 1
-    for i, match in enumerate(player['matches'], 1):
+    for i, match in enumerate(player["info"]["matches"], 1):
         panel.addstr(row, 2 * spaces, "Match #1")
         row += 1
-        panel.addstr(row, 3 * spaces, "Outcome: " + match['outcome'])
+        panel.addstr(row, 3 * spaces, "Outcome: " + match["outcome"])
         row += 1
-        panel.addstr(row, 3 * spaces, "Length: " + match['length'])
+        panel.addstr(row, 3 * spaces, "Length: " + match["length"])
         row += 1
-        panel.addstr(row, 3 * spaces, "Role: " + match['role'])
+        panel.addstr(row, 3 * spaces, "Role: " + match["role"])
         row += 1
-        panel.addstr(row, 3 * spaces, "God: " + match['god'])
+        panel.addstr(row, 3 * spaces, "God: " + match["god"])
         row += 1
-        panel.addstr(row, 3 * spaces, "KDA: " + match['kda'])
+        panel.addstr(row, 3 * spaces, "KDA: " + match["kda"])
         row += 1
+    panel.refresh()
 
 
 def main(
-    names: List[str],
+    players: List[Player],
     screen=curses.initscr(),
 ):
-    assert names
+    assert players
     spaces = 2
     screen.clear()
 
     # new window -> draw names
     screen.addstr("Players:")
-    for i, name in enumerate(names, 1):
-        screen.addstr(i, spaces, name)
+    for i, player in enumerate(players, 1):
+        screen.addstr(i, spaces, player["name"])
     screen.noutrefresh()
-    offset = 1 + len(names)
+    offset = 1 + len(players)
 
     # new windows -> draw info boxes - only name and loading or skipped
     total_width = curses.COLS
     total_height = curses.LINES
 
-    panel_width = int(total_width / len(names))
+    panel_width = int(total_width / len(players))
     panels = [
         curses.newwin(
             total_height - offset,  # nlines
@@ -122,24 +196,29 @@ def main(
             offset,  # begin_y
             i * panel_width,  # begin_x
         )
-        for i in range(len(names))
+        for i in range(len(players))
     ]
-    for panel, name in zip(panels, names):
-        panel.addstr(name)
-        panel.addstr(1, spaces, "Loading...")
+    for panel, player in zip(panels, players):
+        panel.addstr(player["name"])
+        if player["name"] in skipped_names:
+            panel.addstr(1, spaces, "skipped")
+        else:
+            panel.addstr(1, spaces, "loading...")
         panel.noutrefresh()
 
     screen.move(1, spaces)
     curses.doupdate()
 
     # go through names and call hirez api, update panel
-    for panel, name in zip(panels, names):
-        info = call_hirez_api(name)
-        redraw_panel(spaces, name, info, panel)
-        panel.refresh()
+    for panel, player in zip(panels, players):
+        if player["name"] in skipped_names:
+            continue
+        if player["info"] is None:
+            player["info"] = call_hirez_api(player["name"])
+        redraw_panel(spaces, player, panel)
 
     # while true:
-    names_buffer = names.copy()
+    names_buffer = [player["name"] for player in players]
     screen.nodelay(False)
     curses.flushinp()
 
@@ -151,11 +230,12 @@ def main(
         screen.move(y_ + 1, x_ + spaces)
 
     def update_name(y_):
+        screen.move(y_ + 1, 0)  # So that clrtoeol is called on the correct line.
         screen.clrtoeol()
-        if names[y] == names_buffer[y]:
-            screen.addstr(y + 1, spaces, names_buffer[y])
+        if players[y_]["name"] == names_buffer[y_]:
+            screen.addstr(y_ + 1, spaces, names_buffer[y_])
         else:
-            screen.addstr(y + 1, spaces, names_buffer[y] + " (*)")
+            screen.addstr(y_ + 1, spaces, names_buffer[y_] + " (*)")
 
     while True:
         c = screen.get_wch()
@@ -191,23 +271,31 @@ def main(
             if x == 0:
                 continue
             x -= 1
-            names_buffer[y] = names_buffer[y][:x] + names_buffer[y][x + 1:]
+            names_buffer[y] = names_buffer[y][:x] + names_buffer[y][x + 1 :]
             update_name(y)
             set_yx(y, x)
         elif c == curses.KEY_DC:
             y, x = get_yx()
             if x == len(names_buffer[y]):
                 continue
-            names_buffer[y] = names_buffer[y][:x] + names_buffer[y][x + 1:]
+            names_buffer[y] = names_buffer[y][:x] + names_buffer[y][x + 1 :]
             update_name(y)
             set_yx(y, x)
-        elif c == '\n':
-            pass
-        elif c == '\x1B':
+        elif c == "\n":
+            y, _ = get_yx()
+            players[y]["name"] = names_buffer[y]
+            update_name(y)
+            panels[y].clear()
+            panels[y].addstr(players[y]["name"])
+            panels[y].addstr(1, spaces, "loading...")
+            panels[y].refresh()
+            players[y]["info"] = call_hirez_api(players[y]["name"])
+            redraw_panel(spaces, players[y], panels[y])
+        elif c == "\x1B":
             return
         elif isinstance(c, str):
             y, x = get_yx()
-            if len(names_buffer[y]) > total_width - 10:
+            if len(names_buffer[y]) >= 32:
                 continue
             names_buffer[y] = names_buffer[y][:x] + c + names_buffer[y][x:]
             x += len(c)
@@ -271,7 +359,28 @@ def main(
 # if input is a single filename, OCR it
 # otherwise all inputs are playernames
 
+if __name__ == "__main__":
+    names_from_input = [
+        {
+            "name": "Siemka4",
+            "info": None,
+        },
+        {
+            "name": "Jangaru",
+            "info": None,
+        },
+        {
+            "name": "sfdgsfdghsf",
+            "info": None,
+        },
+        {
+            "name": "sfdgsfdghsf",
+            "info": None,
+        },
+        {
+            "name": "sfdgsfdghsf",
+            "info": None,
+        },
+    ]
 
-names_from_input = ["player1", "player2", "player3", "player4", "player5"]
-
-curses.wrapper(lambda x: main(names_from_input, x))
+    curses.wrapper(lambda x: main(names_from_input, x))
